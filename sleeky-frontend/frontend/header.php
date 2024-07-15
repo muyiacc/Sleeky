683647ce016707f2128620f915cd4f57d1036695
muyiacc <115904774+muyiacc@users.noreply.github.com>
2024-07-16 00:19:30 +0800

    Update header.php

--- sleeky-frontend/frontend/header.php
+++ sleeky-frontend/frontend/header.php
@@ -14,6 +14,8 @@
 
         <link rel="stylesheet" href="<?php echo $YOURLS_SITE ?>/frontend/dist/styles.css">
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
         <?php if (defined('backgroundImage')) : ?>
             <style>
                 body {
